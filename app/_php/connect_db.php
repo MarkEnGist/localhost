376d51e3fbe193a98a82f974fc536d1ad1dcc9a2
MarkEnGist <matveivasja@gmail.com>
2018-08-16 00:30:26 +0300

    New commit

--- app/_php/connect_db.php
+++ app/_php/connect_db.php
@@ -5,8 +5,11 @@
      * Date: 15.08.2018
      * Time: 23:46
      */
+    $pdo_connection_options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
     try {
-        $pdo = new PDO('mysql:host=localhost;dbname=test', 'root', 'root');
+        $pdo = new PDO('mysql:host=localhost;dbname=test', 'root', 'root', $pdo_connection_options);
     } catch ( PDOException $e ) {
         echo "Невозможно установить соеднение с базой данных";
     }
